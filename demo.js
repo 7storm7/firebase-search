ae2d9953bf136021df9364f70408b58782d8a482
storm <oguner@gmail.com>
2016-12-18 17:52:33 +0300

    Revert "only with elasticsearch"
    This reverts commit 44746bcaaed2aa69d7bd0e775af7d1c51fa58ecb.

--- demo.js
+++ demo.js
@@ -4,20 +4,29 @@ var axios = require('axios');
 var _ = require('lodash');
 
 firebase.initializeApp({
-  "databaseURL": "https://quiver-firebase-search-ad877.firebaseio.com",
+  "databaseURL": "https://quiver-firebase-search.firebaseio.com",
   "serviceAccount": "./service-account.json"
 });
 
-var proverbsRef = firebase.database().ref('kapakolsun/proverbs');
+var usersRef = firebase.database().ref('demo/users');
 var elasticsearchConfig = {
-    host: '10.128.0.2:9200',
+    host: 'localhost:9200',
     log: 'warning',
-    index: 'kapakolsun'
+    index: 'development'
   };
+var algoliaConfig = require('./env.json').defaults.algolia;
+// Sample Algolia config
+// var algoliaConfig = {
+//   "applicationID": "XXXXXXXX",
+//   "searchAPIKey": "XXXXXXXX",
+//   "monitoringAPIKey": "XXXXXXXX",
+//   "apiKey": "XXXXXXXX"
+// };
  
-var search = new FirebaseSearch(proverbsRef, {
-  elasticsearch: elasticsearchConfig
-}, 'proverbs');
+var search = new FirebaseSearch(usersRef, {
+  elasticsearch: elasticsearchConfig,
+  algolia: algoliaConfig
+}, 'users');
 
 search.elasticsearch.indices.exists()
   .then(function(exists) { // Delete elasticsearch index if it exists
@@ -26,32 +35,69 @@ search.elasticsearch.indices.exists()
   .then(function() { // Create elasticsearch index
     return search.elasticsearch.indices.create();
   })
+  .then(function() { // Check if Algolia index exists
+    return search.algolia.exists();
+  })
+  .then(function(exists) { // Make sure that Algolia index exists
+    return exists ? search.algolia.clearIndex(true) : search.algolia.setSettings({attributesToIndex: ['name', 'gender']});
+  })
   .then(function() { // Set listeners
     search.elasticsearch.firebase.start();
+    search.algolia.firebase.start();
     search.on('all', function(e) {
-      console.log(e.name, e.detail.description, "\n");
+      console.log(e.name, e.detail.name, "\n");
     });
     return true;
   })
   .then(function() {
-    return proverbsRef.remove();
+    return usersRef.remove();
+  })
+  .then(function () { // Download 5 users from SWAPI
+    var i = 5;
+    var promises = [];
+    var users = [];
+    var getUser = function (i) {
+      promises.push(axios.get(`http://swapi.co/api/people/${i + 1}/`)
+        .then(function (res) {
+          users.push(res.data);
+        })
+        .catch(function (err) {
+          console.log('axios err', i);
+          return true;
+        }));
+    };
+
+    while (i--) {
+      getUser(i);
+    }
+    return Promise.all(promises)
+      .then(function() {
+        return users;
+      });
+  })
+  .then(function (users) { // Write users to disk
+    var jsonFormat = require('json-format');
+    var fs = require('fs');
+    var fakeUsersFile = fs.openSync('./fake-users.json', 'w+');
+    fs.writeSync(fakeUsersFile, jsonFormat(users));
+    return fs.closeSync(fakeUsersFile);
   })
   .then(function () { // Read users from disk and push one to Firebase every 1000 millis
     return new Promise(function (resolve, reject) {
-      var proverbs = require('./fake-users.json');
-      var pushProverbs = function (proverb) {
-        proverbsRef.push(proverb)
+      var users = require('./fake-users.json');
+      var pushUser = function (user) {
+        usersRef.push(user)
           .then(function () {
             setTimeout(function () {
-              if (proverbs.length) {
-                pushProverbs(proverbs.pop());
+              if (users.length) {
+                pushUser(users.pop());
               } else {
                 resolve();
               }
             }, 1000);
           });
       };
-      pushProverbs(proverbs.pop());
+      pushUser(users.pop());
     });
   })
   .then(function () {
